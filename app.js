326e1d7fa68b8f58f5e0287d7ba1e49d0885c0f9
Glitch (hello-express) <none>
2021-02-15 06:11:14 +0000

    🚜🎻 Checkpoint ./app.js:27766605/2407

--- app.js
+++ app.js
@@ -62,35 +62,60 @@ app.event('app_home_opened', async ({ event, client, context }) => {
   }
 });
 
+// Listen for a slash command invocation
 app.command('/pechakucha', async ({ ack, payload, context }) => {
   // Acknowledge the command request
   ack();
 
   try {
-    const result = await app.client.chat.postMessage({
+    const result = await app.client.views.open({
       token: context.botToken,
-      // Channel to send message to
-      channel: payload.channel_id,
-      // Include a button in the message (or whatever blocks you want!)
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: 'Go ahead. Click it.'
-          },
-          accessory: {
-            type: 'button',
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: payload.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
             text: {
-              type: 'plain_text',
-              text: 'Click me!'
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
             },
-            action_id: 'button_abc'
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'test_input',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
           }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
         }
-      ],
-      // Text in the notification
-      text: 'Message from Test App'
+      }
     });
     console.log(result);
   }
