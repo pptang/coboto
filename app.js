df1c9bf75112fea14854a339fd1e525d14b5a50c
Glitch (hello-express) <none>
2021-02-19 01:49:53 +0000

    🍛📯 Checkpoint ./app.js:27766605/13019

--- app.js
+++ app.js
@@ -104,6 +104,67 @@ app.event('app_home_opened', async ({ event, client, context }) => {
   }
 });
 
+// Listen for a slash command invocation
+app.command('/coboto', async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  ack();
+
+  try {
+    const result = await app.client.views.open({
+      token: context.botToken,
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: payload.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'test_input',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 
 // Listen for a slash command invocation
 app.command('/coboto', async ({ ack, payload, context }) => {
@@ -123,7 +184,7 @@ app.command('/coboto', async ({ ack, payload, context }) => {
         "type": "modal",
         "title": {
           "type": "plain_text",
-          "text": "Ikigai",
+          "text": "Coboto",
           "emoji": true
         },
         "submit": {
